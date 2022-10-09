65d17162d4b62b2767bec3d0b5315bf55b649ced
David Q Rico <davidqrico@gmail.com>
2022-10-09 15:57:48 -0700

    update

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const dbName = "logreg";
 
 
-mongoose.connect("mongodb+srv://davidqrico:gloZRMIKsjK5JtyO@cluster0.iv4ksfe.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://davidqrico:@cluster0.iv4ksfe.mongodb.net/?retryWrites=true&w=majority")
     .then(()=>{
         console.log(`Connected to the database called ${dbName}`)
     })
